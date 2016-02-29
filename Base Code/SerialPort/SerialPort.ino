bea3c4f5764c5a15f110bcae63ffd828dc05c384
pdllvlle <pdllvlle@memphis.edu>
2016-02-29 14:51:32 -0600

    Fix code again..

--- Base Code/SerialPort/SerialPort.ino
+++ Base Code/SerialPort/SerialPort.ino
@@ -4,20 +4,21 @@
 int command = -1; //stores the command in ACSII code.
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  Serial.println("Please enter 0 or 1 to turn on/off LED on PIN 13";
+  Serial.begin(9600); //Starts serial communication using 9600 baud.
+  pinMode(13, OUTPUT); //Pin 13 contains the LED to control.
+  //Prints intructions to user over the console.
+  Serial.println("Please enter 0 or 1 to turn on/off LED on PIN 13");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if( Serial.available() > 0 ){
-    command = Serial.read();
+  if( Serial.available() > 0 ) { //only if a new command is received. 
+    command = Serial.read(); //Reads the command to ASCII value.
     switch(command){
-      case 48:
-       digitalWrite(13, LOW);
+      case 48: //ASCII code for 0.
+       digitalWrite(13, LOW); //Writes to the pin.
       break;
-      case 49;
+      case 49; //ASCII code for 1. //Writes to the pin.
         digitalWrite(13, HIGH);
       break;
   }
