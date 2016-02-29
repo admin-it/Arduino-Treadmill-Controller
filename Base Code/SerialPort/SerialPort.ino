e22b6fda589da9b4dbb32814b29f91c61652defa
Cannon Moyer <cannonmoyer@ftstoo.com>
2016-02-29 14:58:34 -0600

    commit

--- Base Code/SerialPort/SerialPort.ino
+++ Base Code/SerialPort/SerialPort.ino
@@ -6,18 +6,27 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  Serial.println("Please enter 0 or 1 to turn on/off LED on PIN 13";
+  Serial.println("Please enter 0 or 1 to turn on/off LED on PIN 13");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if( Serial.available() > 0 ){
+  if(Serial.available()){
     command = Serial.read();
     switch(command){
       case 48:
        digitalWrite(13, LOW);
       break;
+      
       case 49;
+        digitalWrite(13, HIGH);
+      break;
+
+      case 50;
+        digitalWrite(13, HIGH);
+      break;
+
+      case 51;
         digitalWrite(13, HIGH);
       break;
   }
