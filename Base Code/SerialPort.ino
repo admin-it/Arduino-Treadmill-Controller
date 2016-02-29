4dcc5f33ef69c61c79609273274fb0b09851f189
pdllvlle <pdllvlle@memphis.edu>
2016-02-29 14:46:33 -0600

    Arduino Base Code

---
+++ Base Code/SerialPort.ino
@@ -0,0 +1,24 @@
+//Created by Enyil Valle (pdllvlle)
+//Basic Serial communication for either bluetooth & cable
+
+int command = -1; //stores the command in ACSII code.
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  Serial.println("Please enter 0 or 1 to turn on/off LED on PIN 13";
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if( Serial.available() > 0 ){
+    command = Serial.read();
+    switch(command){
+      case 48:
+       digitalWrite(13, LOW);
+      break;
+      case 49;
+        digitalWrite(13, HIGH);
+      break;
+  }
+}
